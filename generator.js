e197e00885d45e62e3e39462ce48fee3ff2fd4c1
nicolas377 <68359871+nicolas377@users.noreply.github.com>
2020-11-19 17:53:44 -0500

    initialize

---
+++ generator.js
@@ -0,0 +1,5 @@
+/*["dep","arr","fltnbr",[["waypoint", lat, lon, alt, e, f],*/
+let dep, arr, fltnbr, waypoint, lat, lon, alt
+let e = ''
+let f = ''
+var data = JSON.parse('{"navdata":{"ABAPU":[[-17.82732,19.03378]],"UDBUP":[[52.42778,-55.53145]],"BESEN":[[50.42846,-105.83645]],"NADLU":[[50.49248,-105.76412]],"PENBU":[[50.21247,-105.23255]]}}')
