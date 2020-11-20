d258ae97d20dcea26e33f2271572e9f772d1fae8
nicolas377 <68359871+nicolas377@users.noreply.github.com>
2020-11-19 22:00:48 -0500

    update

--- generator.js
+++ generator.js
@@ -2,4 +2,7 @@
 let dep, arr, fltnbr, waypoint, lat, lon, alt
 let e = ''
 let f = ''
-var data = JSON.parse('{"navdata":{"ABAPU":[[-17.82732,19.03378]],"UDBUP":[[52.42778,-55.53145]],"BESEN":[[50.42846,-105.83645]],"NADLU":[[50.49248,-105.76412]],"PENBU":[[50.21247,-105.23255]]}}')
+var data = JSON.parse('{"ABAPU":[[-17.82732,19.03378]],"UDBUP":[[52.42778,-55.53145]],"BESEN":[[50.42846,-105.83645]],"NADLU":[[50.49248,-105.76412]],"PENBU":[[50.21247,-105.23255]]}')
+function setVar() {
+  //set a variable to a value
+}
